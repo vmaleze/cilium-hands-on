ba8d75de2ac8955b19fa34f3d34121799d2564ff
Manuel PAYET <mpayet@ippon.fr>
2024-05-06 17:30:08 +0200

    ajout provisioning workshop pour windows linux et macos

--- spawn-workshops-ec2/locals.tf
+++ spawn-workshops-ec2/locals.tf
@@ -1,3 +1,25 @@
 locals {
-  ssh_generation_folder="${path.module}/../ssh-pubkeys/"
+  generated_folder="${path.module}/../generated"
+  ssh_generation_folder="${local.generated_folder}/machine-configs/"
+  distribution_folder="${local.generated_folder}/distribution/"
+
+  local_scripts_to_copy = flatten([
+    for instance in var.ec2_instances: [
+      for script in fileset("${path.module}/connection-scripts/", "**"): {
+        key = "${instance}-${script}"
+        content = file("${path.module}/connection-scripts/${script}")
+        filename = "${local.ssh_generation_folder}/${instance}/${script}"
+      }
+    ]
+  ])
+
+    workspace_dirs_to_copy = flatten([
+    for instance in var.ec2_instances: [
+      for folder in var.folders_to_copy_in_workspace: {
+        key = "${instance}-${folder}"
+        folder = folder
+        instance = instance
+      }
+    ]
+  ])
 }
--- spawn-workshops-ec2/terraform.tfvars
+++ spawn-workshops-ec2/terraform.tfvars
@@ -1,5 +1,16 @@
-common_prefix="vmaleze"
+common_prefix="mpayet-test"
 
 ec2_instances= [
-    "marco"
+    "marco",
+    #"polo",
+    #"saxo",
+    #"coco",
+    #"gigo",
+    #"rocco",
+    #"solo",
+    #"pluto",
+    "cacao",
+    "rototo"
 ]
+
+folders_to_copy_in_workspace = [ "../helper-scripts", "../microservices", ".//workshop-preconfigure/.vscode" ]
