e0283366bbb64aeee7c3e67a4af71a97bf5ccb0c
Vivien Maleze <vmaleze@gmail.com>
2024-05-07 15:25:39 +0200

    Get weapon as a string

--- microservices/unit/src/main/java/com/ippon/cilium/handson/unit/clients/Stronghold.java
+++ microservices/unit/src/main/java/com/ippon/cilium/handson/unit/clients/Stronghold.java
@@ -1,6 +1,5 @@
 package com.ippon.cilium.handson.unit.clients;
 
-import com.ippon.cilium.handson.unit.models.Weapon;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
@@ -8,5 +7,5 @@ import retrofit2.http.Header;
 public interface Stronghold {
 
   @GET("/weapons")
-  Call<Weapon> getWeapon(@Header("x-request-sender") String sender);
+  Call<String> getWeapon(@Header("x-request-sender") String sender);
 }
--- microservices/unit/src/main/java/com/ippon/cilium/handson/unit/models/Weapon.java
+++
@@ -1,5 +0,0 @@
-package com.ippon.cilium.handson.unit.models;
-
-public enum Weapon {
-  SWORD, AXE, STAFF;
-}
