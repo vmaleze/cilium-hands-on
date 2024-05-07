229e748d008ec84fd29b07b5cec4c0f170cefeba
Vivien Maleze <vmaleze@gmail.com>
2024-05-07 15:56:05 +0200

    Do not keep connection alive

--- microservices/unit/src/main/java/com/ippon/cilium/handson/unit/clients/Stronghold.java
+++ microservices/unit/src/main/java/com/ippon/cilium/handson/unit/clients/Stronghold.java
@@ -3,9 +3,11 @@ package com.ippon.cilium.handson.unit.clients;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 
 public interface Stronghold {
 
+  @Headers("Connection: close")
   @GET("/weapons")
   Call<String> getWeapon(@Header("x-request-sender") String sender);
 }
